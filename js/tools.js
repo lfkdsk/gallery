65de48fa38beca7d91d62060ebff4d66e26af2dc
lfkdsk <lfkdsk@gmail.com>
2023-10-28 20:11:33 +0000

    deploy: 05a3f2a409541a338aea2558518168f5daa6277f

--- js/tools.js
+++ js/tools.js
@@ -43,3 +43,25 @@ function getParam(key) {
       toastr.info('Please check your clipboard write permission.')
     }
   }
+
+  function simulateDownloadImageClick(uri, filename) {
+    var link = document.createElement('a');
+    if (typeof link.download !== 'string') {
+      window.open(uri);
+    } else {
+      link.href = uri;
+      link.download = filename;
+      accountForFirefox(clickLink, link);
+    }
+  }
+  
+  function clickLink(link) {
+    link.click();
+  }
+  
+  function accountForFirefox(click) { // wrapper function
+    let link = arguments[1];
+    document.body.appendChild(link);
+    click(link);
+    document.body.removeChild(link);
+  }
